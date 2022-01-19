c74c251e8f11b21feb06829c3b5a16c368452b34
André Kovac <me@andrekovac.com>
2022-01-19 15:52:05 +0100

    :monocle_face: show alerts

--- components/PhotoList.tsx
+++ components/PhotoList.tsx
@@ -1,25 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
-import { ActivityIndicator, FlatList, ListRenderItemInfo } from 'react-native';
+import {
+  ActivityIndicator,
+  Alert,
+  FlatList,
+  ListRenderItemInfo,
+  Pressable,
+} from 'react-native';
 
 type ItemT = {
   id: string;
   author: string;
   download_url: string;
+  url: string;
 };
 
-type ItemProps = Pick<ItemT, 'author' | 'download_url'>;
+type ItemProps = Pick<ItemT, 'author' | 'download_url' | 'url'>;
 
-const Item: React.VFC<ItemProps> = ({ author, download_url }) => (
-  <ItemImageWrapper>
-    <ItemImage source={{ uri: download_url }} />
-    <ItemText>by {author.toUpperCase()}</ItemText>
-  </ItemImageWrapper>
+const Item: React.VFC<ItemProps> = ({ author, download_url, url }) => (
+  <Pressable
+    onPress={() => {
+      Alert.alert('Artist', author, [{ text: 'OK' }], { cancelable: false });
+    }}
+    onLongPress={() => {
+      Alert.alert('Url', url, [{ text: 'OK' }], { cancelable: false });
+    }}
+  >
+    {({ pressed }) => (
+      <ItemImageWrapper pressed={pressed}>
+        <ItemImage source={{ uri: download_url }} />
+        <ItemText>by {author.toUpperCase()}</ItemText>
+      </ItemImageWrapper>
+    )}
+  </Pressable>
 );
 
 const renderItem = ({
-  item: { author, download_url },
-}: ListRenderItemInfo<ItemT>) => <Item {...{ author, download_url }} />;
+  item: { author, download_url, url },
+}: ListRenderItemInfo<ItemT>) => <Item {...{ author, download_url, url }} />;
 
 const PhotoList: React.VFC = () => {
   const [isLoading, setLoading] = useState(true);
@@ -65,10 +83,15 @@ const ItemText = styled.Text`
   padding-top: 5px;
 `;
 
-const ItemImageWrapper = styled.View`
+type ItemImageWrapperProps = { pressed: boolean };
+const ItemImageWrapper = styled.View<ItemImageWrapperProps>`
   margin: 10px 0;
-  border: 5px solid #1d1d28;
-  background-color: #1d1d28;
+  ${({ pressed }) =>
+    `
+      opacity: ${pressed ? 0.7 : 1};
+      background-color: ${pressed ? '#3c3c52' : '#1d1d28'};
+      border: 5px solid ${pressed ? '#3c3c52' : '#1d1d28'};
+    `}
 `;
 
 export default PhotoList;
